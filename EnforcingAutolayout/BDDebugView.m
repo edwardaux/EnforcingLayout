e21fffd114d197c71969181cdb9492fefe84d2c1
Craig Edwards <craig@hae.com.au>
2014-04-01 09:36:36 +1100

    Do it totally programmatically - works OK now.

--- EnforcingAutolayout/BDDebugView.m
+++ EnforcingAutolayout/BDDebugView.m
@@ -12,24 +12,45 @@
 @property (nonatomic, strong) IBOutlet NSButton *b1;
 @property (nonatomic, strong) IBOutlet NSButton *b2;
 @property (nonatomic, strong) IBOutlet NSButton *b3;
+@property (nonatomic, strong) IBOutlet NSView *subview;
 @end
 
 @implementation BDDebugView
 
 -(void)updateConstraints {
 	[super updateConstraints];
-	NSLog(@"Before %@", self.constraints);
+
+	// remove the Interface Builder placeholder constraints
+	[self removeConstraints:[self constraints]];
+	[self.subview removeConstraints:[self.subview constraints]];
 	
-	// according to Interface builder this constraint is already there.  However, if you dump out the constraints, it
-	// doesn't exist (and so the third button doesn't have its width linked to the first button).  If we manually add
-	// it here, then everything works just fine.
-	[self addConstraint:[NSLayoutConstraint constraintWithItem:self.b3
-																									 attribute:NSLayoutAttributeWidth
-																									 relatedBy:NSLayoutRelationEqual
-																											toItem:self.b2
-																									 attribute:NSLayoutAttributeWidth
-																									multiplier:1.0
-																										constant:0.0]];
-	NSLog(@"After  %@", self.constraints);
+	//  pin button one
+	[self addConstraint:[NSLayoutConstraint constraintWithItem:self.b1 attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeLeft multiplier:1.0 constant:20.0]];
+	[self addConstraint:[NSLayoutConstraint constraintWithItem:self.b1 attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeTop multiplier:1.0 constant:20.0]];
+	[self addConstraint:[NSLayoutConstraint constraintWithItem:self.b1 attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeRight multiplier:1.0 constant:-60.0]];
+	
+	// pin button two
+	[self addConstraint:[NSLayoutConstraint constraintWithItem:self.b2 attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeLeft multiplier:1.0 constant:20.0]];
+	[self addConstraint:[NSLayoutConstraint constraintWithItem:self.b2 attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.b1 attribute:NSLayoutAttributeBottom multiplier:1.0 constant:20.0]];
+	
+	// pin subview
+	[self addConstraint:[NSLayoutConstraint constraintWithItem:self.subview attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.b2 attribute:NSLayoutAttributeBottom multiplier:1.0 constant:20.0]];
+	[self addConstraint:[NSLayoutConstraint constraintWithItem:self.subview attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeLeft multiplier:1.0 constant:20.0]];
+	[self addConstraint:[NSLayoutConstraint constraintWithItem:self.subview attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeBottom multiplier:1.0 constant:-20.0]];
+	[self addConstraint:[NSLayoutConstraint constraintWithItem:self.subview attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeRight multiplier:1.0 constant:-20.0]];
+	
+	// pin button three
+	[self addConstraint:[NSLayoutConstraint constraintWithItem:self.b3 attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self.subview attribute:NSLayoutAttributeLeft multiplier:1.0 constant:20.0]];
+	[self addConstraint:[NSLayoutConstraint constraintWithItem:self.b3 attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.subview attribute:NSLayoutAttributeTop multiplier:1.0 constant:20.0]];
+	
+	// button heights
+	[self addConstraint:[NSLayoutConstraint constraintWithItem:self.b1 attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0 constant:21.0]];
+	[self addConstraint:[NSLayoutConstraint constraintWithItem:self.b2 attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0 constant:21.0]];
+	[self addConstraint:[NSLayoutConstraint constraintWithItem:self.b3 attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0 constant:21.0]];
+	
+	// button widths
+	[self addConstraint:[NSLayoutConstraint constraintWithItem:self.b2 attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:self.b1 attribute:NSLayoutAttributeWidth multiplier:1.0 constant:0.0]];
+	[self addConstraint:[NSLayoutConstraint constraintWithItem:self.b3 attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:self.b1 attribute:NSLayoutAttributeWidth multiplier:1.0 constant:0.0]];
 }
+
 @end
